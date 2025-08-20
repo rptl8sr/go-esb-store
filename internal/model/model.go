89e8e7dae8a147519932a51c6aeaf0aab07a78ef
pavelborisofff <pavel.borisofff@gmail.com>
2025-08-20 23:28:38 +0300

    add store model

--- internal/model/model.go
+++ internal/model/model.go
@@ -6,3 +6,26 @@ const (
 	Prod Mode = "prod"
 	Dev  Mode = "dev"
 )
+
+type Status string
+
+const (
+	Dead         Status = "Dead"
+	Closed       Status = "Closed"
+	Refranchised Status = "Refranchised"
+	Open         Status = "Open"
+	New          Status = "New"
+	PreOpening   Status = "PreOpening"
+)
+
+type Store struct {
+	Number          int
+	Name            string
+	Address         string
+	Mall            string
+	Company         string
+	Brand           string
+	Format          string
+	Status          Status
+	TemporaryClosed bool
+}
