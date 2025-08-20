54c596d0c42c54963e0337ddde3109fb92d083b0
pavelborisofff <pavel.borisofff@gmail.com>
2025-08-20 21:18:27 +0300

    add esb and client initialization to handler

--- handler.go
+++ handler.go
@@ -2,7 +2,15 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"log/slog"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+
 	"go-esb-store/internal/config"
+	"go-esb-store/internal/esb"
+	"go-esb-store/internal/model"
+	"go-esb-store/pkg/logger"
 	"go-esb-store/pkg/trigger"
 )
 
@@ -15,4 +23,24 @@ func Handler(ctx context.Context, event interface{}) (*Response, error) {
 	cfg := config.Must()
 	triggerType := trigger.DetectType(event)
 
+	logger.Init(cfg.App.LogLevel)
+	if cfg.App.Mode == model.Dev && cfg.App.LogLevel == slog.LevelDebug && triggerType == string(trigger.LocalSource) {
+		fmt.Println("RUNNING IN DEVELOPMENT MODE")
+		fmt.Printf("config: %+v\n", cfg)
+	}
+	logger.Info("main.Handler: Starting...", "trigger_type", triggerType)
+
+	esbClient, err := esb.New(&cfg.ESB)
+	if err != nil {
+		return nil, err
+	}
+	tgClient, err := tgbotapi.NewBotAPI(cfg.Telegram.Token)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Response{
+		StatusCode: 200,
+		Body:       "OK",
+	}, nil
 }
